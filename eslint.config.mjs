2d2ca52423d2a7806f04eaa6869c70e3771d16d5
Zain Ali <zainalis.914@gmail.com>
2026-09-10 09:15:02 +0500

    feat: configure eslint with typescript-eslint support and add necessary dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: ["**/node_modules/**", "**/dist/**", "**/*.config.*"],
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "no-console": "warn",
+    },
+  },
+);
